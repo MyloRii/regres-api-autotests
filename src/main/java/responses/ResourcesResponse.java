ef133940cef2678b0ca2fc4f2c9b695699d6660b
Shumylo, Andrii <Andrii.Shumylo@McKesson.com>
2020-08-24 13:33:12 +0300

    Added ignore properties to ResourceResponse

--- src/main/java/responses/ResourcesResponse.java
+++ src/main/java/responses/ResourcesResponse.java
@@ -1,10 +1,12 @@
 package responses;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class ResourcesResponse {
 
